0a7dc0a96dbd544cb97a414223b6e08e4b2addf2
flatlandnick <nicholas.lunn@flatironschool.com>
2021-08-05 13:58:34 -0500

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  before_action :authorize
+  
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+  private
+
+    def authorize
+       if !session[:user_id]
+        render json: { errors: ["You must be logged in"] }, status: :unauthorized
+       end
+    end
+
+    def record_invalid(e)
+       render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end
